b0045600fec0860afa9f1994ebae3f0469f1edda
micahmaness <manessmic@gmail.com>
2019-02-10 22:54:28 -0500

    Update countThrees.cpp

--- countThrees.cpp
+++ countThrees.cpp
@@ -16,6 +16,7 @@ string fileContents(string file) {
     return file_contents;
 }
 
+// Initiates a string of the numbers and the number of cores
 const string numList = fileContents("nums.txt");
 const int numCores = 8;
 int totalCount = 0;
@@ -26,6 +27,7 @@ int partition(string numbers) {
 
 const int partitionSize = partition(numList);
 
+// Counts the 3s in the string
 int count3s(int id) {
     int start = id * partitionSize;
     string subString = numList.substr(start, partitionSize);
@@ -37,11 +39,14 @@ int count3s(int id) {
     }
     totalCount += count;
 }
-    
+
+// Clocks the time run for the program
+// Partitions based on cores
+// Searches each partition on a different thread
+// Outputs the time and 3s found
 int main() {
     auto startTime = high_resolution_clock::now();
     thread threads[numCores];
-    int totCount = 0;
     for (int i = 0; i < numCores; ++i) {
         threads[i] = thread(count3s, i);
     }
