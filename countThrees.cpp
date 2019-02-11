ba1f2f45fefdc319b862effb6fd8f28f0787a428
micahmaness <manessmic@gmail.com>
2019-02-10 22:48:11 -0500

    Update countThrees.cpp

--- countThrees.cpp
+++ countThrees.cpp
@@ -7,28 +7,30 @@
 #include <fstream>
 #include <chrono>
 using namespace std;
+using namespace std::chrono;
 
-string get_file_contents(string file) {
+// Takes in a text file and turns it into a string
+string fileContents(string file) {
     ifstream infile { file };
     string file_contents { istreambuf_iterator<char>(infile), istreambuf_iterator<char>() };
     return file_contents;
 }
 
-const string CONTENTS = get_file_contents("nums.txt");
-const int CORES = 8;
+const string numList = fileContents("nums.txt");
+const int numCores = 8;
 int totalCount = 0;
 
-int get_thread_partition_size(string numbers) {
-    return numbers.length() / CORES;
+int partition(string numbers) {
+    return numbers.length() / numCores;
 }
 
-const int PARTITION_SIZE = get_thread_partition_size(CONTENTS);
+const int partitionSize = partition(numList);
 
 int count3s(int id) {
-    int start = id * PARTITION_SIZE;
-    string subString = CONTENTS.substr(start, PARTITION_SIZE);
+    int start = id * partitionSize;
+    string subString = numList.substr(start, partitionSize);
     int count = 0;
-    for (int pos = 0; pos < PARTITION_SIZE; ++pos) {
+    for (int pos = 0; pos < partitionSize; ++pos) {
         if (subString[pos] == '3') {
             count++;
         }
@@ -38,19 +40,19 @@ int count3s(int id) {
     
 int main() {
     auto startTime = high_resolution_clock::now();
-    thread threads[CORES];
+    thread threads[numCores];
     int totCount = 0;
-    for (int i = 0; i < CORES; ++i) {
+    for (int i = 0; i < numCores; ++i) {
         threads[i] = thread(count3s, i);
     }
     
-    for (int threadNum = 0; threadNum < CORES; ++threadNum) {
+    for (int threadNum = 0; threadNum < numCores; ++threadNum) {
         threads[threadNum].join();
     }
-    auto endTime = high_resolution_count::now();
-    auto runTime = duration_cast<microseconds>(endTime - startTime);
+    auto endTime = high_resolution_clock::now();
+    auto runTime = duration_cast<milliseconds>(endTime - startTime);
     
     cout << "Number of 3s: " << totalCount << endl;
-    cout << "Run time: " << runTime.count() << " microseconds" << endl;
-    return EXIT_SUCCESS;
+    cout << "Run time: " << runTime.count() << " milliseconds" << endl;
+    return 0;
 }
